6b39bdf4ffc36ab491039f7a6fe096d95d8deb3e
kih1024 <rladlsgh654@naver.com>
2020-11-05 16:06:40 +0900

    Updates

---
+++ precache-manifest.509a222410d525298e6a894eb337e18e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f275941ad6429c775382e449dcf2e5",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "e8e8c680ed8c719a120f",
+    "url": "/movie_app_2020/static/css/main.ca105275.chunk.css"
+  },
+  {
+    "revision": "edadb73d0a80228485be",
+    "url": "/movie_app_2020/static/js/2.145cee8d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movie_app_2020/static/js/2.145cee8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e8c680ed8c719a120f",
+    "url": "/movie_app_2020/static/js/main.40196af6.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
